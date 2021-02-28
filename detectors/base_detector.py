d741a8eae4d26942bcbfca326db0097698ba8bd3
Joshua Hoogstraat <34964599+jhoogstraat@users.noreply.github.com>
2021-02-28 16:08:27 +0100

    Fix base_detector

--- detectors/base_detector.py
+++ detectors/base_detector.py
@@ -1,3 +1,9 @@
+import collections
+
+Object = collections.namedtuple('Object', ['id', 'score', 'bbox'])
+BBox = collections.namedtuple('BBox', ['xmin', 'ymin', 'xmax', 'ymax'])
+
+
 class ObjectDetector(object):
     def labels(self):
         """Produces a compact, json-usable list of labels.
@@ -5,7 +11,7 @@ class ObjectDetector(object):
         Returns:
             List of (int) which maps label id to class name.
         """
-        raise RuntimeError('Must be implemented by subclasses.')
+        raise NotImplementedError
 
     def inputSize(self):
         """Produces a compact, json-usable dict of the model's input size.
@@ -13,12 +19,12 @@ class ObjectDetector(object):
         Returns:
             dict with keys 'w' (width) and 'h' (height) mapping to (int) sizes.
         """
-        raise RuntimeError('Must be implemented by subclasses.')
-    
+        raise NotImplementedError
+
     def detect(self, pilImage):
         """Single frame detection function.
 
         Returns:
             list of namedtuples in shape of ('Object', ['id', 'score', 'bbox']).
         """
-        raise RuntimeError('Must be implemented by subclasses.')
+        raise NotImplementedError
